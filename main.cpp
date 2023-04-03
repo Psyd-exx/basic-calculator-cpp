4f85d4111f3d0a11c80f565c629361aa341cf1a6
Sinan <psydexx64@gmail.com>
2023-04-04 00:29:57 +0100

    feat: add square and square root

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 # include <iostream>
-
+# include <cmath>
 
 using namespace std;
 
@@ -28,47 +28,67 @@ int divide(int numberOne, int numberTwo)
     return numberOne / numberTwo;
 }
 
+int square(int numberOne)
+{
+    return numberOne * numberOne;
+}
+
+int squareRoot(int numberOne)
+{
+    return sqrt(numberOne);
+}
+
+
 int main()
 {
     int numberOne {};
     int numberTwo {};
-    bool calcLoop {true};
     int userChoice {0};
     int result {0};
 
+    cout << "\nWelcome to Sinan's cool C++ Calculator!\n";
+    cout << "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-";
 
-    cout << "Welcome to Sinan's cool C++ Calculator\n";
-    cout << "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n";
-
-    while (userChoice != 5) {
-        cout << "\n\n(1) Add two numbers\n";
+    while (userChoice != 7) {
+        cout << "\n(1) Add two numbers\n";
         cout << "(2) Subtract two numbers\n";
         cout << "(3) Multiply two numbers\n";
         cout << "(4) Divide two numbers\n";
-        cout << "(5) Exit\n";
+        cout << "(5) Square a number\n";
+        cout << "(6) Square root of a number\n";
+        cout << "(7) Exit\n";
         cout << "\nPlease input here: ";
         cin >> userChoice;
 
-        if (userChoice < 1 or userChoice > 5) {
+        if (userChoice < 1 or userChoice > 7) {
             cout << "Invalid Command\n";
-        } else if (userChoice == 5) {
+        } else if (userChoice == 7) {
             cout << "\nExiting Program";
         } else {
             cout << "\nInput the first number: ";
             cin >> numberOne;
-            cout << "\nInput the second number: ";
-            cin >> numberTwo;
 
-            if (userChoice == 1) {
-                result = addition(numberOne, numberTwo);
-            } else if (userChoice == 2) {
-                result = subtraction(numberOne, numberTwo);
-            } else if (userChoice == 3) {
-                result = multiply(numberOne, numberTwo);
-            } else if (userChoice == 4) {
-                result = divide(numberOne, numberTwo);
+            if (userChoice == 5) {
+                result = square(numberOne);
+            } else if (userChoice == 6) {
+                result = squareRoot(numberOne);
             }
 
+
+            if (userChoice != 5 && userChoice != 6) {
+                cout << "\nInput the second number: ";
+                cin >> numberTwo;
+
+                if (userChoice == 1) {
+                    result = addition(numberOne, numberTwo);
+                } else if (userChoice == 2) {
+                    result = subtraction(numberOne, numberTwo);
+                } else if (userChoice == 3) {
+                    result = multiply(numberOne, numberTwo);
+                } else if (userChoice == 4) {
+                    result = divide(numberOne, numberTwo);
+                }
+            }
             cout << "\nThe result is: " << result;
         }
 
