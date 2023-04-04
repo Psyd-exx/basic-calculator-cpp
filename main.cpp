011befc8c7a9432bdb333c244a5a4622d8ef71eb
Sinan <psydexx64@gmail.com>
2023-04-04 01:15:10 +0100

    feat: add exponent option and refactor if/else to switch case

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@
 
 using namespace std;
 
-int addition(int numberOne, int numberTwo)
+int add(int numberOne, int numberTwo)
 {
     return numberOne + numberTwo;
 }
 
-int subtraction(int numberOne, int numberTwo)
+int subtract(int numberOne, int numberTwo)
 {
     return numberOne - numberTwo;
 }
@@ -38,6 +38,14 @@ int squareRoot(int numberOne)
     return sqrt(numberOne);
 }
 
+int exponent(int numberOne, int numberTwo)
+{
+    int tmp {numberOne};
+    for (int i=0; i<numberTwo-1; i++) {
+        tmp = tmp * numberOne;
+    }
+    return tmp;
+}
 
 int main()
 {
@@ -49,50 +57,82 @@ int main()
     cout << "\nWelcome to Sinan's cool C++ Calculator!\n";
     cout << "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-";
 
-    while (userChoice != 7) {
+    while (userChoice != 8) {
         cout << "\n(1) Add two numbers\n";
         cout << "(2) Subtract two numbers\n";
         cout << "(3) Multiply two numbers\n";
         cout << "(4) Divide two numbers\n";
         cout << "(5) Square a number\n";
         cout << "(6) Square root of a number\n";
-        cout << "(7) Exit\n";
+        cout << "(7) Number one raised to the power of number two\n";
+        cout << "(8) Exit\n";
         cout << "\nPlease input here: ";
         cin >> userChoice;
 
-        if (userChoice < 1 or userChoice > 7) {
-            cout << "Invalid Command\n";
-        } else if (userChoice == 7) {
-            cout << "\nExiting Program";
-        } else {
-            cout << "Input the first number: ";
-            cin >> numberOne;
-
-            if (userChoice == 5) {
-                result = square(numberOne);
-            } else if (userChoice == 6) {
-                result = squareRoot(numberOne);
-            }
-
-
-            if (userChoice != 5 && userChoice != 6) {
+        switch (userChoice) {
+            case 1:
+                cout << "Input the first number: ";
+                cin >> numberOne;
                 cout << "Input the second number: ";
                 cin >> numberTwo;
-
-                if (userChoice == 1) {
-                    result = addition(numberOne, numberTwo);
-                } else if (userChoice == 2) {
-                    result = subtraction(numberOne, numberTwo);
-                } else if (userChoice == 3) {
-                    result = multiply(numberOne, numberTwo);
-                } else if (userChoice == 4) {
-                    result = divide(numberOne, numberTwo);
+                result = add(numberOne, numberTwo);
+                cout << "RESULT -> " << result << "\n";
+                break;
+            case 2:
+                cout << "Input the first number: ";
+                cin >> numberOne;
+                cout << "Input the second number: ";
+                cin >> numberTwo;
+                result = subtract(numberOne, numberTwo);
+                cout << "RESULT -> " << result << "\n";
+                break;
+            case 3:
+                cout << "Input the first number: ";
+                cin >> numberOne;
+                cout << "Input the second number: ";
+                cin >> numberTwo;
+                result = multiply(numberOne, numberTwo);
+                cout << "RESULT -> " << result << "\n";
+                break;
+            case 4:
+                cout << "Input the first number: ";
+                cin >> numberOne;
+                cout << "Input the second number: ";
+                cin >> numberTwo;
+                result = divide(numberOne, numberTwo);
+                if (result == -1) {
+                    cout << "Cannot divide by zero! \n";
+                } else {
+                    cout << "RESULT -> " << result << "\n";
                 }
-            }
-            cout << "RESULT -> " << result << "\n";
+                break;
+            case 5:
+                cout << "Input the number to square: ";
+                cin >> numberOne;
+                result = square(numberOne);
+                cout << "RESULT -> " << result << "\n";
+                break;
+            case 6:
+                cout << "Input the number to take the square root of: ";
+                cin >> numberOne;
+                result = squareRoot(numberOne);
+                cout << "RESULT -> " << result << "\n";
+                break;
+            case 7:
+                cout << "Input the base number: ";
+                cin >> numberOne;
+                cout << "Input the exponent: ";
+                cin >> numberTwo;
+                result = exponent(numberOne, numberTwo);
+                cout << "RESULT -> " << result << "\n";
+                break;
+            case 8:
+                cout << "\nExiting Program";
+                break;
+            default:
+                cout << "Invalid Command\n";
+                break;
         }
-
     }
-
     return 0;
 }
